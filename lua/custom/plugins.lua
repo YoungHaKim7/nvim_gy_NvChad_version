d354b48520fb85c3b55c1eaf1122baaee09f70fc
GYoung <ytok1108@aol.com>
2024-01-28 12:39:22 +0900

    VnChad Ver.. add plugins

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -60,6 +60,22 @@ local plugins = {
   --   "mg979/vim-visual-multi",
   --   lazy = false,
   -- }
+  -- -----------
+  -- My Plugins
+  
+  -- nvim-surround 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  }
+
+
 }
 
 return plugins
